be5025ab5db0b55c1b704ec577655387da7d4df4
SamsonFlomo <flomosamson125@gmail.com>
2024-11-01 16:54:10 +0000

    first instance

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Container from 'react-bootstrap/Container';
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <Container>
+
+      <h1>AirBnB Experience</h1>
+
+    </Container>
+  )
+}
+
+export default App
